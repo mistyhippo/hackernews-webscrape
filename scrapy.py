228e6937192235d8794dc7940c5bb2d83914c0a4
mistyhippo <Valentinabrent@gmail.com>
2020-07-04 16:41:32 -0700

    import pprint,  discard links without votes, only include linkns with 100+ votes

--- scrapy.py
+++ scrapy.py
@@ -1,8 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 votes = soup.select('.score')
-print(votes[0].get('id'))
+subtext = soup.select('.subtext')
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return hn
+
+
+pprint.pprint(create_custom_hn(links, subtext))
